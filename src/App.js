6e5cb9637e99e059f9ef3f6ac2c8823c9a0fdd96
Bianca Ritorto <bianritorto@fb.com>
2022-05-15 19:56:28 -0300

    Background and dark mode

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import InviteeForm from './InviteeForm';
-import { ReactComponent as Invitation } from './invitation.svg';
+import { ReactComponent as Invitation } from './invitation-3.svg';
 import Stack from '@mui/material/Stack';
-
-
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './App.css';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const invitationStyle = {
+  minHeight: 400,
+  maxHeight: 600,
+};
+
 function App() {
-  const invitationStyle = {
-    height: 700,
-  };
+
 
   return (
-    <div className="App" >
-      <Stack spacing={4} sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center' }}>
-        <Invitation style={invitationStyle} />
-        <InviteeForm></InviteeForm>
-      </Stack>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="App" >
+        <Stack spacing={4} sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center' }}>
+          <Invitation style={invitationStyle} />
+          <InviteeForm></InviteeForm>
+        </Stack>
+      </div>
+    </ThemeProvider>
   );
 }
 
