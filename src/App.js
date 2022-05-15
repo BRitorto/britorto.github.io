784af38683ad558a1fd966222a61f235d9bc86fd
Bianca Ritorto <bianritorto@fb.com>
2022-05-15 11:06:08 -0300

    React app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import InviteeForm from './InviteeForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <InviteeForm></InviteeForm>
+    </div>
+  );
+}
+
+export default App;
